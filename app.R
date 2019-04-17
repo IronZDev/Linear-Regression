28506013167321be252134b4bde26a960d5eb456
IronZaphilith <maciek.stokfisz@gmail.com>
2019-04-17 21:29:26 +0200

    Added columns selection and handling of reavtive variavbles

--- app.R
+++ app.R
@@ -8,63 +8,88 @@
 #
 
 library(shiny)
+library(shinyjs)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Linear Regression"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-        fileInput("file", 
-                  label = h3("File input"),
-                  multiple = FALSE,
-                  accept = c(".dat")
-        ),
-        selectInput("select", label = h3("Select box"), 
-                    choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3), 
-                    selected = 1)
-        
-        # hr(),
-        # fluidRow(column(3, verbatimTextOutput("value")))
+  
+  useShinyjs(),
+  
+  # Application title
+  titlePanel("Linear Regression"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("file", 
+                label = h3("Provide .csv file"),
+                multiple = FALSE,
+                accept = c(".dat")
       ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-        tableOutput("fileContent")
-         # plotOutput("distPlot")
+      disabled(
+        selectInput("select", label = h3("Choose target category: "), 
+                    choices = NULL, 
+                    selected = -1),
+        actionButton("startBtn", "Calculate!")
       )
-   )
+      # hr(),
+      # fluidRow(column(3, verbatimTextOutput("value")))
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      tableOutput("fileContent")
+      # plotOutput("distPlot")
+    )
+  )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
+  data = reactiveVal(0) # Global variable for content of csv
   
-   output$fileContent <- renderTable({
-     # Check if file exists
-     if (is.null(input$file))
-       return(NULL)
-     
-     # Check if file has .dat extension
-     extension = strsplit(input$file$name, "\\.")
-     if (extension[[1]][2] != "csv") {
-       print("Wrong file format!")
-       return(NULL)
-     }
-     
-     return(read.csv(file = input$file$datapath, header = TRUE, sep=","))
-   })
+  observeEvent(input$file, {
+    if (is.null(input$file)) {
+      disable("select")
+      disable("startBtn")
+      return(NULL)
+    }
+    
+    # Check if file has .dat extension
+    extension = strsplit(input$file$name, "\\.")
+    if (extension[[1]][2] != "csv") {
+      alert("Wrong file format!")
+      reset("file")
+      disable("select")
+      disable("startBtn")
+      return(NULL)
+    }
+    content = read.csv(file = input$file$datapath, header = TRUE, sep=",")
+    colnames(content) <- gsub(".", " ", colnames(content), fixed=TRUE) # Replace "." with " " in column names
+    updateSelectInput(session, "select", NULL, choices = colnames(content)) # Update options in selectInput
+    data(content) # Save to reactive variable
+    enable("select")
+    enable("startBtn")
+  })
   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  observeEvent(input$startBtn, {
+    print(data()[[input$select]])
+  })
+  
+  # output$fileContent <- renderTable({
+  #   # Check if file exists
+  #   if (is.null(input$file))
+  #     return(NULL)
+  #   
+  #   # Check if file has .dat extension
+  #   extension = strsplit(input$file$name, "\\.")
+  #   if (extension[[1]][2] != "csv") {
+  #     print("Wrong file format!")
+  #     return(NULL)
+  #   }
+  #   
+  #   return(read.csv(file = input$file$datapath, header = TRUE, sep=","))
+  # })
 }
 
 # Run the application 
