3c142944e2a86cba017fd4c520eaa746942ecd6c
IronZaphilith <maciek.stokfisz@gmail.com>
2019-05-13 02:13:44 +0200

    Fixed resetting select box Added pretty alerts Added comments

--- app.R
+++ app.R
@@ -1,14 +1,12 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
+# Intelligent Systems
+# Deusto University
+# Maciej Stokfisz, 2019
 #
 
 library(shiny)
 library(shinyjs)
+library(shinyalert)
 library(caret)
 library(ggplot2)
 
@@ -16,18 +14,21 @@ library(ggplot2)
 ui <- fluidPage(
   
   useShinyjs(), # Using shinyJS for disabling and enabling elements
+  useShinyalert(), # For pretty alerts
   
   # Application title
   titlePanel("Linear Regression"),
   
-  # Sidebar with a slider input for number of bins 
+  # Sidebar 
   sidebarLayout(
     sidebarPanel(
+      # Upload box
       fileInput("file", 
                 label = h3("Provide .csv file"),
                 multiple = FALSE,
                 accept = c(".dat")
       ),
+      # Select category
       disabled(
         selectInput("select", label = h3("Choose target category: "), 
                     choices = NULL, 
@@ -36,34 +37,39 @@ ui <- fluidPage(
       )
     ),
     
-    # Show a plot of the generated distribution
+    # Show calculated mean absolute error + graph
     mainPanel(
       h3(textOutput("MAE"), align="center"),
       plotOutput("maePlot")
     )
   )
 )
+a
 
-# Define server logic required to draw a histogram
 server <- function(input, output, session) {
   data = reactiveVal(0) # Global variable for content of csv
   
+  # Updating UI
   observeEvent(input$file, {
+    # If not input file then disable category select and start button
     if (is.null(input$file)) {
       disable("select")
       disable("startBtn")
       return(NULL)
     }
     
-    # Check if file has .dat extension
+    
+    # Check if file has .csv extension
     extension = strsplit(input$file$name, "\\.")
     if (extension[[1]][2] != "csv") {
-      alert("Wrong file format!")
+      reset("select")
       reset("file")
-      disable("select")
+      delay(100, disable("select")) # Delay to make sure that it will be resetted first
       disable("startBtn")
+      shinyalert("Wrong file format!", type="error")
       return(NULL)
     }
+    
     content = read.csv(file = input$file$datapath, header = TRUE, sep=",")
     colnames(content) <- gsub(".", " ", colnames(content), fixed=TRUE) # Replace "." with " " in column names
     updateSelectInput(session, "select", NULL, choices = colnames(content)) # Update options in selectInput
@@ -72,28 +78,31 @@ server <- function(input, output, session) {
     enable("startBtn")
   })
   
+  # Start button clicked event
   observeEvent(input$startBtn, {
+    # Generate indices for partitioning
     indices = createMultiFolds(y=data()[[input$select]],5,1)
     MAEs = vector() # Vector of MAEs
     for (index in  1:length(indices)) {
+      # Do partitioning
       training.data = data()[indices[[index]], ] 
       test.data = data()[-indices[[index]], ]
       target = training.data[[input$select]]
       training.data[[input$select]] = NULL # Delete the column to be predicted from taining data
-      print(cor(training.data))
+      # print(cor(training.data))
       model = lm(target~., training.data)
-      print(summary(model))
+      # print(summary(model))
       predictions = predict(model, test.data)
+      # Calcuate MAE and add to vector
       MAE = mean(abs(predictions - test.data[[input$select]]))
       MAEs = c(MAEs, MAE)
     }
     # print(mean(MAEs))
-    output$category = renderText({
-      return(input$select)
-    })
+    
     output$MAE = renderText({
       return(paste0("Overall mean absolute error: ",mean(MAEs)))
     })
+    
     output$maePlot = renderPlot({
       plotData = data.frame(num = 1:length(MAEs), MAE = MAEs)
       ggplot(plotData, aes(x=num, y=MAE))+
